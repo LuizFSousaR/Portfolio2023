7314ab3cdc5890aa5d8542e362bbae34e2d22585
LuizFLoopie <luiz.rodrigues@loopie.com.br>
2023-05-26 13:52:46 -0300

    Commit Herança
    - Herança de Repositório antigo;

---
+++ Scripts/lightmodeScript.js
@@ -0,0 +1,8 @@
+const toogleSwitch = document.querySelector('.toogleSwitch');
+
+toogleSwitch.addEventListener('click',  function() {
+    document.body.classList.toggle('bodyDark');
+
+    //aqui fudeu tudo
+    document.html.body.section.toggle('menuDark');
+})
